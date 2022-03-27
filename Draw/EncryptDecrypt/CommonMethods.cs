9f9fb8f5d98a6509927f0a8f1c93557928749374
Shirley Cohen <shilychanxd@hotmail.com>
2022-03-27 14:31:58 +0300

    added new cryptography
    cryptographic hash function - MD5

--- Draw/EncryptDecrypt/CommonMethods.cs
+++ Draw/EncryptDecrypt/CommonMethods.cs
@@ -3,31 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Draw.EncryptDecrypt
 {
     public static class CommonMethods
     {
-        public static string Key = "ardj#@@nfjf!lkj";
+
+        public static string hash = "jsj#kf@2ak*m^";
 
         public static string ConvertToEncrypt(string password)
         {
-            if (string.IsNullOrEmpty(password)) return "";
+            //converting the string to byte array
+            byte[] data = UTF8Encoding.UTF8.GetBytes(password);
+            using(MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                //the ComputeHash methods of MD5CryptoServiceProvider class returns the hash as an array of 16 bytes
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
 
-            password += Key;
-            var passwordBytes = Encoding.UTF8.GetBytes(password);
-            return Convert.ToBase64String(passwordBytes);
+                //create a new TripleDESCryptoServiceProvider object to generate a key and initialization vector 
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7})
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    var encrypted_password = Convert.ToBase64String(results, 0, results.Length);
+                    return encrypted_password;
+                }
+            }
         }
+
 
 
         public static string ConvertToDecrypt(string base64EncodeData)
         {
-            if (string.IsNullOrEmpty(base64EncodeData)) return "";
+            byte[] data = Convert.FromBase64String(base64EncodeData);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
 
-            var base64EncodeBytes = Convert.FromBase64String(base64EncodeData);
-            var result = Encoding.UTF8.GetString(base64EncodeBytes);
-            result = result.Substring(0, result.Length - Key.Length);
-            return result;
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();//changing to .CreateDecryptor()
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    var decrypted_password = UTF8Encoding.UTF8.GetString(results); // converting the byte array to string
+                    return decrypted_password;
+                }
+            }
         }
 
     }
